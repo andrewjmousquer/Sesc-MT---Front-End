940f500bf21cb44269b3ee65223648e6433bbf9b
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-16 15:01:52 -0400

    build: add editorconfig

--- src/pages/Home/Programming/index.js
+++ src/pages/Home/Programming/index.js
@@ -56,7 +56,7 @@ export default function Programming() {
             <div
               onClick={() => moveDot(index + 1)}
               className={slideIndex === index + 1 ? "dot active" : "dot"}
-             />
+            />
           ))}
         </div>
       </div>
