9722e682de7e589a180707950ad5f9536c79e726
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-17 15:39:02 -0400

    build: add font

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -1,15 +1,6 @@
-import "./styles.scss";
 import "../../App.scss";
+import "./styles.scss";
 
 export function Services() {
-  return (
-    <div className="wrapper">
-      <h1>Using Sass in Reacttt</h1>
-      <header className="wrapper__btns">
-        <button>Blue Button</button>
-        <button>Red Button</button>
-        <button>Green Button</button>
-      </header>
-    </div>
-  );
+  return <h1>SESC MT</h1>;
 }
