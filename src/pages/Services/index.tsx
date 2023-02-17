3da1aa5f74d5a6f6795deeaaeb342b0e0c4242ac
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-17 10:24:57 -0400

    feat: sass config

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -1,3 +1,15 @@
+import "./styles.scss";
+import "../../App.scss";
+
 export function Services() {
-  return <h1>Servicos</h1>;
+  return (
+    <div className="wrapper">
+      <h1>Using Sass in Reacttt</h1>
+      <header className="wrapper__btns">
+        <button>Blue Button</button>
+        <button>Red Button</button>
+        <button>Green Button</button>
+      </header>
+    </div>
+  );
 }
