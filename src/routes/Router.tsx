cf3c0e327a015779d87125668c83890faf2d05fa
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-17 09:22:10 -0400

    refact: routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Services } from "../pages/Services/Services";
+import { Services } from "../pages/Services";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Services />} />
+      <Route path="/servicos" element={<Services />} />
     </Routes>
   );
 }
