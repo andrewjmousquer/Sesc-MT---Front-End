87a106799a671576b480baf543f7e1f29fc7f1a9
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-20 17:28:47 -0400

    feat: turn card into components

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,25 @@
+import "./styles.scss";
+
+const img = require("../../assets/img/Rectangle 49.png");
+
+// interface CardProps {
+//   id?: Number;
+//   backgroundColor?: String;
+//   title?: String;
+//   description?: String;
+// }
+
+export function Card(props: any) {
+  return (
+    <a href="/">
+      <div className="card">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title m-0 pt-2 pb-3">{props.title}</h5>
+          <p className="card-text m-0 pb-4">{props.description}</p>
+          <span>+ detalhes</span>
+        </div>
+      </div>
+    </a>
+  );
+}
--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -1,5 +1,43 @@
-import { Cards } from "./Cards";
+import { Card } from "../../components/Card";
+import { PaginationDefault } from "../../components/Pagination";
+
+const cards = [
+  {
+    id: 1,
+    backgroundColor: "#fbfdff",
+    title: "Portal da Academia",
+    description:
+      "Movimentos que trabalham todo o corpo para fortalecimento e estabilização dos músculos, utilizando aparelhos próprios para a  atividade.",
+  },
+  {
+    id: 2,
+    backgroundColor: "#00162a",
+    title: "Home",
+    description: "Utilizando aparelhos próprios para a  atividade.",
+  },
+];
 
 export function Services() {
-  return <Cards />;
+  return (
+    <>
+      <section className="container my-5">
+        <section className="row">
+          {cards.map((card) => {
+            return (
+              <section key={card.id} className="col col-md-6 col-lg-4 mb-5">
+                <Card
+                  key={card.id}
+                  title={card.title}
+                  description={card.description}
+                  backgroundColor={card.backgroundColor}
+                />
+              </section>
+            );
+          })}
+        </section>
+      </section>
+
+      <PaginationDefault />
+    </>
+  );
 }
