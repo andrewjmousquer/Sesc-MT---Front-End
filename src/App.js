dbb88e8a33162395b0b49e5cce5beba8a31d9851
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-22 15:00:49 -0400

    refact: App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
+
 import { Router } from "./routes/Router";
 
 import "./App.css";
-import HomeView from "./assets/COLÔNIA DE FÉRIAS SESC - 2022.mp4";
-import home_Logo from "./assets/Icon_Search.png";
-import Sesc_Logo from "./assets/logo Sesc.png";
-import setaHome from "./assets/Seta_Vector.png";
-import Hero from "./pages/Home/Hero";
-import Navbar from "./components/Navbar";
-import Slider from "./pages/Home/Slider";
-import Programming from "./pages/Home/Programming";
-import Footer from "./components/Footer";
-import News from "./pages/Home/News";
-import QuickAccess from "./pages/Home/QuickAccess";
+// import HomeView from "./assets/COLÔNIA DE FÉRIAS SESC - 2022.mp4";
+// import home_Logo from "./assets/Icon_Search.png";
+// import Sesc_Logo from "./assets/logo Sesc.png";
+// import setaHome from "./assets/Seta_Vector.png";
+// import Hero from "./pages/Home/Hero";
+// import Navbar from "./components/Navbar";
+// import Slider from "./pages/Home/Slider";
+// import Programming from "./pages/Home/Programming";
+// import Footer from "./components/Footer";
+// import News from "./pages/Home/News";
+// import QuickAccess from "./pages/Home/QuickAccess";
 
 function App() {
   return (
