954757c7e194891b45120b99ab2969e43013be01
andrewjmousquer <andrewjmousquer@gmail.com>
2023-02-24 11:22:47 -0400

    Hero add HOME

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -1,6 +1,9 @@
-import "./styles.css";
+import "./styles.scss";
 
-function Hero(imageSrc: any, imgSeta: any) {
+export function Hero() {
+
+  const imageSrc = require("../../../assets/video/COLÔNIA DE FÉRIAS SESC - 2022.mp4");
+
   return (
     <div className="hero">
       <video autoPlay loop muted src={imageSrc} className="hero__image" />
@@ -10,9 +13,7 @@ function Hero(imageSrc: any, imgSeta: any) {
         desenvolvimento diversas habilidades: motoras, sociai e emocionais.
       </p>
       <p className="hero_footer">Role a página para baixo</p>
-      <img src={imgSeta} alt="seta" className="seta__image" />
     </div>
   );
 }
 
-export default Hero;
---
+++ src/pages/Programing/index.tsx
@@ -0,0 +1,20 @@
+﻿import { Footer } from '../../components/Footer'
+import { Navbar } from '../../components/Navbar'
+import './style.scss'
+
+ export function Programming() {
+
+    return (
+        <div>
+          <Navbar />
+
+          Hello World
+
+          <Footer />
+        </div>
+    )
+}
+
+
+
+
