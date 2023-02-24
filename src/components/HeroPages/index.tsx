4da1807fc63fba7c8011d885e88573e142d56a9c
Adolfo Cruz <adolfo.cruz@gaotech.com.br>
2023-02-24 10:02:00 -0400

    refact: HeroPages

--- src/components/HeroPages/index.tsx
+++ src/components/HeroPages/index.tsx
@@ -5,15 +5,15 @@ import imageSrc from "../../assets/img/image-1.png";
 export function HeroPages() {
   return (
     <div className="hero" style={{ backgroundImage: `url(${imageSrc})` }}>
-      <div className="content">
-        <h1 className="title mb-2">SERVIÇOS</h1>
+      <div className="hero__content">
+        <h1 className="hero__content__title mb-2">SERVIÇOS</h1>
 
-        <p className="subtitle mb-0">
+        <p className="hero__content__subtitle mb-0">
           Veja os serviços que oferemos hoje no SESC-MT
         </p>
       </div>
 
-      <div className="overlay" />
+      <div className="hero__overlay" />
     </div>
   );
 }
